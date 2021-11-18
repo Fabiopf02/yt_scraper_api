b863c01369d7a988116d1a78ea786e87f83f15ca
Fabiopf02 <fabiofranca593@gmail.com>
2021-11-18 09:29:00 -0300

    expose headers

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(cors({ exposedHeaders: 'X-Total-Count' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 app.use(routes);
 
